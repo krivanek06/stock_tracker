767273b2d186d85ddeeac221a980535c079bfaab
Eduard Krivanek <47821225+krivanek06@users.noreply.github.com>
2020-07-10 11:29:27 +0200

    dashbord refactored, going on details

--- frontend/src/app/features/stock-tracker/stock-tracker.module.ts
+++ frontend/src/app/features/stock-tracker/stock-tracker.module.ts
@@ -2,38 +2,34 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { SharedModule } from "../../shared/shared.module";
 import { TableTopCardComponent } from "./components/table-top-card/table-top-card.component";
-import { DashboardTopTableContainerComponent } from "./container/dashboard-top-table-container/dashboard-top-table-container.component";
 import { StoreModule } from "@ngrx/store";
 import { EffectsModule } from "@ngrx/effects";
 import { StockWatchList } from "./store/stockWatchList/stockWatchList.effects";
 import { WatchlistModalContainerComponent } from "./container/modal/watchlist-selector-modal-container/watchlist-modal-container.component";
 import { TableWatchlistComponent } from "./components/table-watchlist/table-watchlist.component";
 import { WatchlistTablesContainerComponent } from "./container/watchlist-tables-container/watchlist-tables-container.component";
-
-// reducers
-import * as fromStockTracker from "./store";
 import { ArticleCardsComponent } from "./components/article-cards/article-cards.component";
 import { EarningsCardComponent } from "./components/earnings-card/earnings-card.component";
-import {DashboardChartContainerComponent} from './container/dashboard-chart-container/dashboard-chart-container.component';
 import {FinancialChartCardModalContainerComponent} from './container/modal/financial-chart-card-modal-container/financial-chart-card-modal-container.component';
 import {StockPriceRangeCardComponent} from './components/stock-price-range-card/stock-price-range-card.component';
 import {StockPriceCardComponent} from './components/stock-price-card/stock-price-card.component';
-import {DashboardNewsContainerComponent} from './container/dashboard-news-container/dashboard-news-container.component';
+
+// reducers
+import * as fromStockTracker from "./store";
+import {FinancialChartContainerComponent} from './container/financial-chart-container/financial-chart-container.component';
 
 @NgModule({
   declarations: [
     TableTopCardComponent,
-    DashboardTopTableContainerComponent,
     WatchlistModalContainerComponent,
     TableWatchlistComponent,
     WatchlistTablesContainerComponent,
     ArticleCardsComponent,
     EarningsCardComponent,
-    DashboardChartContainerComponent,
     FinancialChartCardModalContainerComponent,
     StockPriceRangeCardComponent,
     StockPriceCardComponent,
-    DashboardNewsContainerComponent
+    FinancialChartContainerComponent
   ],
   imports: [
     CommonModule,
@@ -43,17 +39,15 @@ import {DashboardNewsContainerComponent} from './container/dashboard-news-contai
   ],
   exports: [
     TableTopCardComponent,
-    DashboardTopTableContainerComponent,
     WatchlistModalContainerComponent,
     TableWatchlistComponent,
     WatchlistTablesContainerComponent,
     ArticleCardsComponent,
     EarningsCardComponent,
-    DashboardChartContainerComponent,
     FinancialChartCardModalContainerComponent,
     StockPriceRangeCardComponent,
     StockPriceCardComponent,
-    DashboardNewsContainerComponent
+    FinancialChartContainerComponent
   ],
   entryComponents: [
     WatchlistModalContainerComponent,
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,17 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import {AppModule} from './app/app.module';
+import {environment} from './environments/environment';
 
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
+
+    if (window) {
+        window.console.log = () => {
+        };
+    }
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+    .catch(err => console.log(err));
