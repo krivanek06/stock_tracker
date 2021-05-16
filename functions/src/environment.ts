567f9422df77c2b7fcf180b5c3bc5abab6857e31
krivanek06 <krivanek1234@gmail.com>
2021-05-16 16:44:59 +0200

    added environments for functions

---
+++ functions/src/environment.ts
@@ -0,0 +1,11 @@
+
+export const stockDataAPI = 'http://127.0.0.1:5000';  // local dev flask
+//export const stockDataAPI = 'http://127.0.0.1:5001/api/market_data'; // outside docker container
+//export const stockDataAPI = 'http://nginx:5001/api/market_data';    // inside docker container
+//export const stockDataAPI = 'https://serverflask-eqvqg22pdq-lz.a.run.app'; // GCP
+
+// "stock-tracker-common-interfaces": "file:../common-interfaces" / "1.0.7"
+
+export const IS_PRODUCTION = false;
+
+
