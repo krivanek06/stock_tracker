51a3292a0a6084a9df3cdcfb98d557019635f97c
Eduard Krivanek <eduard.krivanek@cloudtalk.io>
2021-10-07 09:27:33 +0200

    fixed news

--- client/src/app/features/market-feature/components/market-stock-news/market-stock-news.component.ts
+++ client/src/app/features/market-feature/components/market-stock-news/market-stock-news.component.ts
@@ -1,19 +1,23 @@
-import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
-import {StfmStockNew} from '@core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { StfmStockNew } from '@core';
+import { BREAK_POINTS } from '@shared';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
-    selector: 'app-market-stock-news',
-    templateUrl: './market-stock-news.component.html',
-    styleUrls: ['./market-stock-news.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+	selector: 'app-market-stock-news',
+	templateUrl: './market-stock-news.component.html',
+	styleUrls: ['./market-stock-news.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MarketStockNewsComponent implements OnInit {
-    @Input() stockArticle: StfmStockNew;
+	@Input() stockArticle: StfmStockNew;
+	isSMDownScreen$: Observable<boolean>;
 
-    constructor() {
-    }
+	constructor(private breakpointObserver: BreakpointObserver) {}
 
-    ngOnInit() {
-    }
-
+	ngOnInit() {
+		this.isSMDownScreen$ = this.breakpointObserver.observe([BREAK_POINTS.SM_DOWN]).pipe(map((x) => x.matches));
+	}
 }
