344c74e1af3e8e6df400a3270ebe7d7614d1ae8b
krivanek06 <krivanek1234@gmail.com>
2021-10-09 20:35:23 +0200

    fix

--- client/src/app/core/api/graphql-account.service.ts
+++ client/src/app/core/api/graphql-account.service.ts
@@ -109,6 +109,7 @@ export class GraphqlAccountService {
 									portfolioCash: STARTING_PORTFOLIO,
 								},
 								transactionsSnippets: [],
+								topTransactions: [],
 							},
 						},
 					});
