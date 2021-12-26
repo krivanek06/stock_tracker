4991df4dd6cfd0a2dab351e1ecf7a7d9b181ed2d
Eduard Krivanek <eduard.krivanek@cloudtalk.io>
2021-12-26 13:39:41 +0100

    cliend: fixed trading

--- client/src/app/features/stock-trading-feature/entry-components/trade-confirmation-pop-over/trade-confirmation-pop-over.module.ts
+++ client/src/app/features/stock-trading-feature/entry-components/trade-confirmation-pop-over/trade-confirmation-pop-over.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
 import { IonicModule } from '@ionic/angular';
 import { FormMatInputWrapperModule } from '@shared';
 import { TradingBidAskModule } from './../../components/trading';
@@ -8,7 +9,7 @@ import { TradeConfirmationPopOverComponent } from './trade-confirmation-pop-over
 
 @NgModule({
 	declarations: [TradeConfirmationPopOverComponent],
-	imports: [IonicModule, CommonModule, ReactiveFormsModule, TradingBidAskModule, FormMatInputWrapperModule],
+	imports: [IonicModule, CommonModule, ReactiveFormsModule, TradingBidAskModule, FormMatInputWrapperModule, MatDialogModule],
 	exports: [TradeConfirmationPopOverComponent],
 })
-export class TradeConfirmationPopOverModule {}
+export class TradeConfirmationPopOverModule { }
