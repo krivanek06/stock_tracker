bb2e2abc2540c4cd11a82c085b643ff7fbccf817
krivanek06 <krivanek1234@gmail.com>
2022-05-01 14:28:47 +0200

    fix(client): fixed stock details valuation

--- client/src/app/shared/components/items/index.ts
+++ client/src/app/shared/components/items/index.ts
@@ -2,5 +2,4 @@ export * from './position-change-item/position-change-item.module';
 export * from './price-change-item/price-change-item.module';
 export * from './price-compare-item/price-compare-item.module';
 export * from './stock-info-identification-item/stock-info-identification-item.module';
-export * from './table-increasing-item/table-increasing-item.module';
 export * from './title-value-item/title-value-item.module';
