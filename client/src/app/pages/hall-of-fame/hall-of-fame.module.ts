97c0212bb57a52634ed88979ef285b58b2d65127
Eduard Krivanek <krivanek1234@gmail.com>
2022-07-22 10:12:12 +0300

    feat(client): hall-of-fame removed pages

--- client/src/app/pages/hall-of-fame/hall-of-fame.module.ts
+++ client/src/app/pages/hall-of-fame/hall-of-fame.module.ts
@@ -1,32 +1,34 @@
+import { AccountOverviewDialogModule, UserAccountInfoListModule } from '@account-feature';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
 import { RouterModule, Routes } from '@angular/router';
-import { IonicModule } from '@ionic/angular';
+import { GroupTopUsersInformationModule } from '@group-feature';
+import { LoaderWrapperModule, PositionCardModule, SharedModule } from '@shared';
+import { HallOfFameUsersBestModule } from './hall-of-fame-users/hall-of-fame-users-best/hall-of-fame-users-best.module';
+import { HallOfFameUsersWorstModule } from './hall-of-fame-users/hall-of-fame-users-worst/hall-of-fame-users-worst.module';
 import { HallOfFamePage } from './hall-of-fame.page';
 const routes: Routes = [
 	{
 		path: '',
 		component: HallOfFamePage,
-		children: [
-			{
-				path: '',
-				redirectTo: 'users',
-				pathMatch: 'full',
-			},
-			{
-				path: 'users',
-				loadChildren: () => import('./hall-of-fame-users/hall-of-fame-users.module').then((m) => m.HallOfFameUsersModule),
-			},
-			{
-				path: 'groups',
-				loadChildren: () => import('./hall-of-fame-groups/hall-of-fame-groups.module').then((m) => m.HallOfFameGroupsModule),
-			},
-		],
 	},
 ];
 
 @NgModule({
-	imports: [CommonModule, RouterModule.forChild(routes), IonicModule],
+	imports: [
+		CommonModule,
+		SharedModule,
+		RouterModule.forChild(routes),
+		UserAccountInfoListModule,
+		PositionCardModule,
+		GroupTopUsersInformationModule,
+		AccountOverviewDialogModule,
+		LoaderWrapperModule,
+		HallOfFameUsersWorstModule,
+		HallOfFameUsersBestModule,
+		MatDividerModule,
+	],
 	declarations: [HallOfFamePage],
 })
 export class HallOfFamePageModule {}
